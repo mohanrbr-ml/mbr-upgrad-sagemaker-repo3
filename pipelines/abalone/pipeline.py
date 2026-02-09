fd01c71fd573ea284a04b5af3dacfc1233aff341
mohanrbr-ml <rkr393@gmail.com>
2026-02-09 15:36:29 +0530

    Changed to m4

--- pipelines/abalone/pipeline.py
+++ pipelines/abalone/pipeline.py
@@ -130,8 +130,8 @@ def get_pipeline(
     model_package_group_name="AbalonePackageGroup",
     pipeline_name="AbalonePipeline",
     base_job_prefix="Abalone",
-    processing_instance_type="ml.t3.medium",
-    training_instance_type="ml.t3.medium",
+    processing_instance_type="ml.t4.large",
+    training_instance_type="ml.t4.large",
 ):
     """Gets a SageMaker ML Pipeline instance working with on abalone data.
 
